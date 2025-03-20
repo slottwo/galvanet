868d1a5f0275f234786a9723da862b56559d9c7d
slottwo <41028091+slottwo@users.noreply.github.com>
2025-03-19 23:01:46 -0300

    Refactor app.py
    Signed-off-by: slottwo <41028091+slottwo@users.noreply.github.com>

--- src/galvanet/app.py
+++ src/galvanet/app.py
@@ -1,27 +1,30 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-import uvicorn
 
+# Application
 app = FastAPI()
 
+# List to store all connected clients
 connections: list[WebSocket] = []
 
 
+# Root endpoint
 @app.get('/')
 async def root():
     with open("src/index.html") as file:
         return HTMLResponse(file.read())
 
 
+# WebSocket endpoint
 @app.websocket('/ws')
 async def ws_endpoint(websocket: WebSocket):
     await websocket.accept()
     connections.append(websocket)
     try:
         while True:
-            msg = await websocket.receive_text()
+            message: str = await websocket.receive_text()
             for connection in connections:
-                await connection.send_text(f"User {websocket.client.host} says: {msg}")
+                await connection.send_text(f"User says: {message}")
     except Exception as e:
         connections.remove(websocket)
         print("Conexção encerrada:", e)
