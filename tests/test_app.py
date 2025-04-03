32580042379ec1ba09fac75983a1ea01949303ab
slottwo <41028091+slottwo@users.noreply.github.com>
2025-04-02 23:49:13 -0300

    Clear, cover and reorganize tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,42 +2,81 @@ from http import HTTPStatus
 
 
 def test_root_ok(client):
-    # Arrange
     html = open("src/index.html", encoding="utf-8").read()
 
-    # Act
     response = client.get("/")
 
-    # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.text == html
 
 
-def test_users_create(client):
-    # Act
+def test_user_create_created(client):
     response = client.post(
         "/users/",
-        json={"username": "testusername", "password": "testpassword"},
+        json={"username": "test", "password": "0000"},
     )
 
-    # Assert
     assert response.status_code == HTTPStatus.CREATED
-    assert response.json() == {"id": 1, "username": "testusername"}
+    assert response.json() == {"id": 1, "username": "test"}
 
 
-def test_users_read(client):
-    # Act
+def test_users_read_ok(client):
     response = client.get("/users/")
 
-    # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         "users": [
-            {"id": 1, "username": "testusername"},
+            {"id": 1, "username": "test"},
         ]
     }
 
 
+def test_user_read_ok(client):
+    response = client.get("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"id": 1, "username": "test"}
+
+
+def test_user_read_not_found(client):
+    response = client.get("/users/2")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_user_update_ok(client):
+    response = client.put(
+        "/users/1", json={"username": "t3st", "password": "0000"}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"id": 1, "username": "t3st"}
+
+
+def test_user_update_not_found(client):
+    response = client.put(
+        "/users/2", json={"username": "t3st", "password": "0000"}
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_users_delete_ok(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"id": 1, "username": "t3st"}
+
+
+def test_users_delete_not_found(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
 # def test_ws_endpoint(client: TestClient):
-#     # Act
+#
 #     client.websocket_connect()
