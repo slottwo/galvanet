235bc3d175bbfa1bd1317a8175160d72dd6d8b4e
slottwo <41028091+slottwo@users.noreply.github.com>
2025-04-03 00:33:59 -0300

    Resolve id initiation requirement error

--- src/galvanet/models.py
+++ src/galvanet/models.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
-table_registry = registry()
+table_registry: registry = registry()
 
 
 @table_registry.mapped_as_dataclass
 class User:
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
