f11d74c7b84cc76471e3f05c7075e8e27245e48f
slottwo <41028091+slottwo@users.noreply.github.com>
2025-04-02 23:48:42 -0300

    Users CRUD methods, remove fake db

--- src/galvanet/app.py
+++ src/galvanet/app.py
@@ -1,9 +1,9 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, HTTPException, WebSocket
 from fastapi.responses import HTMLResponse
 
-from galvanet.schemas import UserDB, UserList, UserPublic, UserSchema
+from galvanet.schemas import UserList, UserPublic, UserSchema
 
 # Application
 app = FastAPI()
@@ -11,48 +11,59 @@ app = FastAPI()
 # List to store all connected clients
 connections: list[WebSocket] = []
 
-# Provisory database
-database = []
+
+# HTTP endpoints
 
 
-# Root endpoint
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     with open("src/index.html", encoding="utf-8") as file:
         return HTMLResponse(file.read())
 
 
-# Users endpoints
 @app.get("/users/", response_model=UserList)
 def read_users():
-    return {"users": database}
-
-
-@app.get("/users/{user_id}", response_model=UserPublic)
-def read_user(user_id: int):
-    for user in database:
-        if user.id == user_id:
-            return user
-    return HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='user not found')
+    return {"users": ...}
 
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_w_id = UserDB(id=1 + len(database), **user.model_dump())
-    database.append(user_w_id)
-    return user_w_id
+    ...  # create user
+    return ...  # created user
+
+
+@app.get("/users/{user_id}", response_model=UserPublic)
+def read_user(user_id: int):
+    if ...:  # check user_id
+        return ...  # user
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+    )
 
 
 @app.put("/users/{user_id}", response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
-    user_w_id = UserDB(id=user_id, **user.model_dump())
-    for k, v in enumerate(database):
-        if v.id == user_id:
-            database[k] = user_w_id
-            return user_w_id
+    if ...:  # check user_id
+        ...  # update user
+        return ...  # updated user
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+    )
 
 
-# WebSocket endpoint
+@app.delete("/users/{user_id}", response_model=UserPublic)
+def delete_user(user_id: int, user: UserSchema):
+    if ...:  # check user_id
+        ...  # delete user
+        return ...  # deleted user
+    raise HTTPException(
+        status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+    )
+
+
+# Websockets endpoints
+
+
 @app.websocket("/ws")
 async def ws_endpoint(websocket: WebSocket):
     await websocket.accept()
