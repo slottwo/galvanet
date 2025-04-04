65fc50494ee6f159f23befa3d3eb2f663466865f
slottwo <41028091+slottwo@users.noreply.github.com>
2025-04-04 18:21:08 -0300

    Add type hints and the test's automation cleanup_connections

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,25 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from galvanet.app import app
+from galvanet.app import app, connections
 from galvanet.models import table_registry
 from galvanet.settings import Settings
 
 
 @pytest.fixture
-def client():
+def client() -> TestClient:
     return TestClient(app)
 
 
+@pytest.fixture(autouse=True)
+def cleanup_connections():
+    connections.clear()
+    yield
+    connections.clear()
+
+
 @pytest.fixture
-def session():
+def session() -> Session:
     # Arrange
     engine = create_engine(Settings().DATABASE_URL)
     table_registry.metadata.create_all(engine)
