ed4e8b54371866e39c365410c5019cb91fdade44
slottwo <41028091+slottwo@users.noreply.github.com>
2025-05-08 10:43:20 -0300

    Override client session for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,31 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import Session
 
 from galvanet.app import app, connections
+from galvanet.database import get_session
 from galvanet.models import table_registry
 from galvanet.settings import Settings
 
 
 @pytest.fixture
-def client() -> TestClient:
-    return TestClient(app)
+def client(session) -> TestClient:
+    
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+
+        yield client
+    
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture(autouse=True)
-def cleanup_connections():
+def cleanup_connections() -> None:
     connections.clear()
     yield
     connections.clear()
@@ -23,7 +34,11 @@ def cleanup_connections():
 @pytest.fixture
 def session() -> Session:
     # Arrange
-    engine = create_engine(Settings().DATABASE_URL)
+    engine = create_engine(
+        "sqlite:///:memory",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
