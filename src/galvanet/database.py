9ea36f4cae70a7482a133e521814254e7dd4a962
slottwo <41028091+slottwo@users.noreply.github.com>
2025-05-08 12:09:40 -0300

    Format database.py

--- src/galvanet/database.py
+++ src/galvanet/database.py
@@ -5,6 +5,7 @@ from galvanet.settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
 
+
 def get_session() -> Session:  # pragma: no cover
     with Session(engine) as session:
         yield session
